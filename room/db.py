4848d2288dc52a75cac6d8e704421bc353173d79
Rahul Kumar <rahul.k@panorbit.in>
2021-05-04 21:17:41 +0530

    fix new

--- room/db.py
+++ room/db.py
@@ -8,12 +8,12 @@ password = urllib.parse.quote(MANGO_JWT_SETTINGS['db_pass'])
 username = urllib.parse.quote(MANGO_JWT_SETTINGS['db_user'])
 db_name = MANGO_JWT_SETTINGS['db_name']
 db_host_mongo = MANGO_JWT_SETTINGS['db_host']
-db_port_mongo = MANGO_JWT_SETTINGS['db_port']
+# db_port_mongo = MANGO_JWT_SETTINGS['db_port']
 # 'username': DB_USERID_MONGO,
 # 'password': DB_PASSWORD_MONGO,
 host= db_host_mongo ,
 database = MANGO_JWT_SETTINGS['db_name']
-port = MANGO_JWT_SETTINGS['db_port']
+# port = MANGO_JWT_SETTINGS['db_port']
 
 
 if 'db_port' in MANGO_JWT_SETTINGS:
@@ -24,9 +24,10 @@ if 'db_port' in MANGO_JWT_SETTINGS:
         username=username, password=password, db_host=db_host_mongo,
         db_port_mongo=db_port_mongo, db_name=db_name)
 else:
+    print("hello")
     mongo_uri = "mongodb+srv://{username}:{password}@{host}/{db_name}".format(
         username=username, password=password, host=db_host_mongo, db_name=db_name)
 
 
-client = MongoClient()
+client = MongoClient(mongo_uri)
 database = client[db_name]
